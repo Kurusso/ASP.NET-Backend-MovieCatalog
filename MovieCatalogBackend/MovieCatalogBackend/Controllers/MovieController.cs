ae078436ec0955ef9d4197d827d9a06831ec8a75
Alexey <lesha.braun1029@gmail.com>
2022-10-20 13:53:12 +0700

    add FilmPageGetService
    MovieController created and FilmPageGetService implemented

---
+++ MovieCatalogBackend/MovieCatalogBackend/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieCatalogBackend.Models.DTO;
+using MovieCatalogBackend.Services;
+
+namespace MovieCatalogBackend.Controllers
+{
+    [Route("api/movies/")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private IFilmPageGetService _filmPageGetService;
+        public MovieController(IFilmPageGetService filmPageGetService)
+        {
+            _filmPageGetService = filmPageGetService;
+        }
+
+        [HttpGet("{page}")]
+        public async Task<IActionResult> GetPage(int page)
+        {
+
+            try
+            {
+                var PageModel = await _filmPageGetService.GetFilmsOnPage(page);
+
+                return Ok(new
+                {
+                    Movies = PageModel.MovieElements,
+                    PageInfo = PageModel.PageInfoModel
+                }
+            );
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+    }
+}
+
--- MovieCatalogBackend/MovieCatalogBackend/Migrations/20221020051623_Movies.Designer.cs
+++ MovieCatalogBackend/MovieCatalogBackend/Migrations/20221020051623_Movies.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieCatalogBackend.Context;
 
@@ -11,9 +12,10 @@ using MovieCatalogBackend.Context;
 namespace MovieCatalogBackend.Migrations
 {
     [DbContext(typeof(MovieCatalogDbContext))]
-    partial class MovieCatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221020051623_Movies")]
+    partial class Movies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
